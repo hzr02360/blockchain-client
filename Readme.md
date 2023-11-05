1f0b2a56599643079ff0d7a8ededfd9ab158c5b7
hzr02360 <bravo-gr@nifty.com>
2023-11-05 17:13:35 +0900

    リファクタ（パラメータをファイル化）

--- Readme.md
+++ Readme.md
@@ -22,6 +22,8 @@ $
 - 生成した鍵を post_transaction.py に反映する
 
 ```bash
-$ python post_transaction.py
+$ python post_transaction.py local_b.yml
+{'time': '2023-11-05T17:06:49.854296', 'sender': 'd3f9cb2f7d217d6d5295cc61c64caf63dcb2ad6f49baca4be967fa29d2214e25be99eaaf084c32aae81d1ef941ee89fac6b98b8740e2457f8457bb9627a9141c', 'reciever': 'a1e1f4542b3ff5b4ebea0921825e3198115ab9cc2674ca0b6ae9da3e8217227610fd2f3714e51452d6e4603e4bf2b6c3cdf3a29df7e7cd3188e466c13fbd1957', 'amount': 222, 'description': 'B -> A', 'signature': '8821dc89639ee519090d4a3ae4461adaef3fe0fbdbde4bb0475d4f54fcd4858b04f8b00e7b7e7b26a2c466634a817ee1b7ba76637b1da42ac4696dc491384604'}
+URL: http://127.0.0.1:8002/put_transaction/
 $
 ```
--- post_transaction.py
+++ post_transaction.py
@@ -1,7 +1,11 @@
 # トランザクションを登録するクライアント機能
+import os
+import sys
 import requests
 import json
 import datetime
+
+import yaml
 
 from credential import create_secret_key, create_signeture
 
@@ -28,24 +32,32 @@ def create_transaction(secret_key, sender_pub_key, reciever_pub_key, amount, des
   }
   return singed_tran
 
-def main():
-  # secret_key = "fa6148d0bbff1d93c744cb3f4e1763445fb0c5e548c3c4c2b4e1d875790d4d62"
-  # sender_pub_key = "a1e1f4542b3ff5b4ebea0921825e3198115ab9cc2674ca0b6ae9da3e8217227610fd2f3714e51452d6e4603e4bf2b6c3cdf3a29df7e7cd3188e466c13fbd1957"
-  # reciever_pub_key = "d3f9cb2f7d217d6d5295cc61c64caf63dcb2ad6f49baca4be967fa29d2214e25be99eaaf084c32aae81d1ef941ee89fac6b98b8740e2457f8457bb9627a9141c"
-  secret_key = "2456056f9a0606197e3977033f6bd8a04f5b039f1f4865cb4f98e239dba1aee1"
-  sender_pub_key = "d3f9cb2f7d217d6d5295cc61c64caf63dcb2ad6f49baca4be967fa29d2214e25be99eaaf084c32aae81d1ef941ee89fac6b98b8740e2457f8457bb9627a9141c"
-  reciever_pub_key = "a1e1f4542b3ff5b4ebea0921825e3198115ab9cc2674ca0b6ae9da3e8217227610fd2f3714e51452d6e4603e4bf2b6c3cdf3a29df7e7cd3188e466c13fbd1957"
-  amount = 222
-  desc = "description"
-  url = "http://127.0.0.1:8001/put_transaction/"
-  # url = "https://blockchain-1-q4494128.deta.app/put_transaction"
-  # url = "https://blockchain-3-q4494128.deta.app/put_transaction"
-  # url = "https://bc-sample.onrender.com/put_transaction"
-  # url = "https://bc-api-0z7g.onrender.com/put_transaction"
+def check_arg():
+  if (len(sys.argv) <= 1):
+    print("parameter file is required.")
+    sys.exit(4)
 
-  transaction = create_transaction(secret_key, sender_pub_key, reciever_pub_key, amount, desc)
-  res = requests.post(url, json.dumps(transaction))
-  print(res)
+  yaml_file = sys.argv[1]
+  if os.path.exists(yaml_file) == False:
+    print(f"{yaml_file} is not found.")
+    sys.exit(4)
+  return yaml_file
+
+def main():
+  yaml_file = check_arg()
+  with open(yaml_file, 'r') as f:
+    arg = yaml.safe_load(f)
+    
+  transaction = create_transaction(
+    arg['secret_key'],
+    arg['sender_pub_key'],
+    arg['reciever_pub_key'],
+    arg['amount'],
+    arg['desc'])
+  print(transaction)
+  print(f"URL: {arg['url']}")
+  #res = requests.post(arg['url'], json.dumps(transaction))
+  #print(res)
 
 if __name__ == "__main__":
   main()
